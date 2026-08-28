c46c4ad735a03a1377f7a6a91426b3b97c828888
malikovakmal478-prog <malikovakmal478@gmail.com>
2026-08-28 19:37:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def deploy_bot():
         chat_histories[user_email].append({"prompt": prompt, "token": bot_token})
 
         api_key = os.environ.get("GEMINI_API_KEY")
-        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={api_key}"
+        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={api_key}"
         
         payload = {
             "contents": [{
